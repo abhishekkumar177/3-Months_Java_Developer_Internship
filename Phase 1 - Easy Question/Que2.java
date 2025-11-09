f2bcb63c4e847af7d351dc052563c22549f3b30d
Abhishek Kumar <abhishekumar1772004@gmail.com>
2025-11-09 07:29:30 +0530

    Create Que2.java

---
+++ Phase 1 - Easy Question/Que2.java
@@ -0,0 +1,18 @@
+/* Take a Number as Input, check whether the number is even or odd. */
+
+import java.util.Scanner;
+
+public class Que2{
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the Number: ");
+    int number = nextInt();
+    if (number % 2 = 0) {
+      System.out.print("The" +number+ "is an Even.");
+    }
+    else {
+      System.out.print("The" +number+ "is Odd");
+    }
+    scanner.close();
+  }
+}
