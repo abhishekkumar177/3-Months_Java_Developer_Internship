db2c59b23e63c9cca443e1202d7a1ea09701eb01
abhishekkumar177 <abhishekumar1772004@gmail.com>
2025-11-09 07:07:27 +0530

    init

---
+++ Phase 1 - Easy Question/Que1.java
@@ -0,0 +1,19 @@
+/* Take a number as input and print whether it is positive, negative, or zero. */
+
+import java.util.Scanner;
+
+public class Que1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number:"  );
+        double number = scanner.nextDouble();
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+        scanner.close();
+    }
+}
