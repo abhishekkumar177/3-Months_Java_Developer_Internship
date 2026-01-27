8682e601291057141ca09875be0b6da21bc744b5
Abhishek Kumar <abhishekumar1772004@gmail.com>
2026-01-27 23:23:50 +0530

    Create Larger.java

---
+++ Logic/Phase 1/Section 1/Larger.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Larger{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter number: ");
+        int num2 = sc.nextInt();
+        
+        if (num1>num2) {
+            System.out.println("First One");
+        } else {
+            System.out.println("First One");
+        }
+        sc.close();
+    }
+}
